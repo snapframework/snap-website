271615209be157ee86071baaa6783d37eae356c1
James Sanders <jimmyjazz14@gmail.com>
2010-04-11 19:00:18 -0400

    Various style experiments

--- w2/builder.hs
+++ w2/builder.hs
@@ -1,11 +1,12 @@
 import Text.RSTemplate
 import System.FilePath
 import System.Environment
+import Data.String.Utils
 import qualified Data.ByteString.Char8 as C
 
 loadEnv fp = do 
   l <- readFile fp >>= return . lines
-  return $ map ((\(a,b)->(a,tail b)) . break (== ':')) l
+  return $ map ((\(a,b)->(strip a,strip $ tail b)) . break (== ':')) l
                 
 
 buildFile env fp = do
