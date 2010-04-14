5e364157d234e3c9416add7a20b2c76850d5c448
James Sanders <james.sanders@thisismedium.com>
2010-04-14 12:05:28 -0400

    Updated build script a little, added favicon

--- w2/build.sh
+++ w2/build.sh
@@ -5,7 +5,7 @@ build="./builder env.conf"
 set -x
 
 ghc --make ./builder.hs || exit 1
-rsync -auP src/ site/
+rsync -auP --delete src/ site/
 
 $build src/index.html > site/index.html
 $build src/media/css/main.css > site/media/css/main.css
