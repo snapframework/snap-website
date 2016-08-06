6fd3db9b3db21698c96f9054a9728cee04471d69
Joshua Cough <joshcough@gmail.com>
2016-08-06 16:38:26 -0400

    git submodules, init-sandbox.sh

---
+++ init-sandbox.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+cabal sandbox init
+cabal sandbox add-source deps/io-streams
+cabal sandbox add-source deps/io-streams-haproxy
+cabal sandbox add-source deps/snap-core
+cabal sandbox add-source deps/snap-server
+cabal sandbox add-source deps/xmlhtml
+cabal sandbox add-source deps/heist
+cabal sandbox add-source deps/snap-static-pages
+cabal sandbox add-source deps/snap
